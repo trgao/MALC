93ede5b920de2a8562b5774a428c053dc3bdca8b
trgao <112098096+trgao@users.noreply.github.com>
2024-04-19 10:54:55 +0800

    Initial Commit

---
+++ MALC/MALCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MALCApp.swift
+//  MALC
+//
+//  Created by Gao Tianrun on 19/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MALCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
